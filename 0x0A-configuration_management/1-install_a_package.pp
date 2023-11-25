073a739ae7f507008edef219a18364cbc7595650
YousefShata <yossef_shata_a@yahoo.com>
2023-11-25 04:42:53 -0800

    why it is not wokring

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Install Flask version 2.1.0 using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+package { 'flask':
+provider => 'pip3',
+ensure   => '2.1.0'
 }

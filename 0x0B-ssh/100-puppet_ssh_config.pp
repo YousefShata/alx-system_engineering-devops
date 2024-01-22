5f3f1cd575cd2374217a95d41e10e680f1a0a996
YousefShata <yossef_shata_a@yahoo.com>
2024-01-21 18:23:05 -0800

    puppet ssh config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# puppet_ssh_config.pp
+
+file { '/root/.ssh/config':
+  ensure  => 'present',
+  content => "
+Host 54.160.106.244
+    IdentityFile /root/.ssh/school
+    PreferredAuthentications publickey
+    PasswordAuthentication no",
+}

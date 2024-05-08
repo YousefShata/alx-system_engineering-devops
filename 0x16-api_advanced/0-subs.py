5095c03f78a0598c0c71abd958a08f9e50ffc90b
YousefShata <yossef_shata_a@yahoo.com>
2024-05-07 17:08:53 -0700

    i dunno

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,15 +12,17 @@ def number_of_subscribers(subreddit):
     """
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = { 
+    headers = {
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
     }
     res = requests.get(url, headers=headers, allow_redirects=False)
 
+    print("Response status code:", res.status_code)  # Print response status code for debugging
+
     if res.status_code == 200:
         data = res.json()
         subs = data['data']['subscribers']
-        
+
         return subs
-    
+
     return 0
